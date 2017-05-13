b6afa7e9f288db271ab4d605eb2ef5469999d586
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-13 14:08:49 -0400

    feat: added a square root functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -32,7 +32,10 @@ class ViewController: UIViewController {
         if let mathematicalSymbol = sender.currentTitle {
             switch mathematicalSymbol {
             case "π":
-                display!.text = "3.1415926"
+                display!.text = String(Double.pi)
+            case "√":
+                let operand = Double(display!.text!)!
+                display!.text = String(sqrt(operand))
             default:
                 break
             }
