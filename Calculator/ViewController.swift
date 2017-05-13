d30088fb35137a42a451083c3eeca9d676ed88a0
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-13 14:23:55 -0400

    clean: cleaned up optionals and converting displayValue

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,18 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else {
-            display!.text = digit
+            display.text = digit
             userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
         }
     }
     
@@ -32,10 +41,9 @@ class ViewController: UIViewController {
         if let mathematicalSymbol = sender.currentTitle {
             switch mathematicalSymbol {
             case "π":
-                display!.text = String(Double.pi)
+                displayValue = Double.pi
             case "√":
-                let operand = Double(display!.text!)!
-                display!.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
             default:
                 break
             }
